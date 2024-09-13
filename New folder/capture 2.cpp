ec04fafdb8f408df0094e454f8b5af7f7744e987
nurooj691 <nurooj691@gmail.com>
2024-09-13 15:41:00 +0500

    Add files via upload

---
+++ New folder/capture 2.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	int num1, num2;
+	printf("enter first number");
+	scanf("%d", &num1 );
+	printf("enter second number");
+	scanf("%d", &num2);
+	if (num1>num2){
+	
+	if(num1>100){
+	
+		printf("first number is significantly larger");
+		
+		
+		
+	}
+	
+}
+     else if(num1<num2){
+	 
+     if(num1<0){
+	 
+     printf("first number is negative and smaller");
+     
+     
+ }
+ }
+     else if(num1==num2){
+     	printf("both are equal");
+	 }
+
+	return 0;
+}
